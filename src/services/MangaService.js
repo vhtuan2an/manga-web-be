2a3d91e2346b8235dc1cd5bd88a703b7dabf9b32
Võ Hoàng Tuấn <166472565+vhtuan2an@users.noreply.github.com>
2025-11-11 16:53:00 +0700

    feat: add thumbnail and updatedAt field in chapter list

--- src/services/MangaService.js
+++ src/services/MangaService.js
@@ -259,7 +259,7 @@ class MangaService {
   async getChapterList(mangaId) {
     try {
       const chapters = await Chapter.find({ mangaId: mangaId })
-        .select("title chapterNumber _id")
+        .select("title chapterNumber _id thumbnail updatedAt")
         .sort({ chapterNumber: 1 })
         .lean();
       if (!chapters) {
