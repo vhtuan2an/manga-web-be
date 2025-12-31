b37135f2dae7e6827cd229064bcabd8b21f4feed
Võ Hoàng Tuấn <166472565+vhtuan2an@users.noreply.github.com>
2025-12-31 23:43:46 +0700

    fix: remove pages field in chapter list

--- src/services/MangaService.js
+++ src/services/MangaService.js
@@ -425,7 +425,7 @@ class MangaService {
   async getChapterList(mangaId) {
     try {
       const chapters = await Chapter.find({ mangaId: mangaId })
-        .select("title chapterNumber _id thumbnail pages createdAt updatedAt")
+        .select("title chapterNumber _id thumbnail createdAt updatedAt")
         .sort({ chapterNumber: 1 })
         .lean();
       if (!chapters) {
